00f8feb6b1d4f9f44d867f0f03c27fa3cb537a72
tanmoynandy <tanmoynandy@rocketmail.com>
2021-01-10 19:51:33 +0530

    added another println statement in main class

--- helloworlddocker/src/main/java/helloworlddocker/HelloWorldDocker.java
+++ helloworlddocker/src/main/java/helloworlddocker/HelloWorldDocker.java
@@ -3,5 +3,6 @@ package helloworlddocker;
 public class HelloWorldDocker {
 	public static void main(String[] args) {
 		System.out.println("Hello World Docker!!!");
+		System.out.println("This is another statement");
 	}
 }
